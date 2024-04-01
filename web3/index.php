d227acf231ab6b71c8212669596fbadb61c3fcc8
abegacheva <144003753+abegacheva@users.noreply.github.com>
2024-04-01 22:14:04 +0300

    Update index.php

--- web3/index.php
+++ web3/index.php
@@ -3,16 +3,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $user = 'u67311'; 
     $pass = '5681522'; 
     $db = new PDO('mysql:host=localhost;dbname=u67311', $user, $pass);
-    $name = $_POST['fio'];
+    $fio = $_POST['fio'];
     $phone = $_POST['phone'];
-    $email = $_POST['email'];
+    $mail = $_POST['email'];
     $birthdate = $_POST['birthdate'];
     $gender = isset($_POST['gender']) ? $_POST['gender'] : '';
     $bio = $_POST['bio'];
     echo "<div class='error-message-container'>";
     $errors = [];
 
-    if (!preg_match("/^[a-zA-Zа-яА-Я ]+$/u", $name)) {
+    if (!preg_match("/^[a-zA-Zа-яА-Я ]+$/u", $fio)) {
         $errors[] = "Введите корректное имя.";
     }
     if (!preg_match("/^\d{4}-\d{2}-\d{2}$/", $birthdate)) {
@@ -26,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $errors[] = "Введите корректный номер телефона";
     }
 
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
         $errors[] = "Введите корректный email";
     }
     if (empty($gender)) {
@@ -37,17 +37,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             echo "<div error-message style='color: red;'>$error</div>";
         }
     } else {
-        $stmt = $db->prepare("INSERT INTO App (FIO, Phone, Email, Birthdate, Gender, Bio) VALUES (?, ?, ?, ?, ?, ?)");
-        $stmt->execute([$name, $phone, $email, $birthdate, $gender, $bio]);
+        $stmt = $db->prepare("INSERT INTO Users (FIO, Phone, Email, Birthdate, Gender, Bio) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->execute([$fio, $phone, $mail, $birthdate, $gender, $bio]);
         $last_id = $db->lastInsertId();
         if (isset($_POST['programming_languages']) && !empty($_POST['programming_languages'])) {
             foreach ($_POST['programming_languages'] as $language) {
-                $stmt = $db->prepare("SELECT ID FROM Ability WHERE ProgrammingLanguage = ?");
+                $stmt = $db->prepare("SELECT ID FROM Languages WHERE ProgrammingLanguage = ?");
                 $stmt->execute([$language]);
                 $ability_id = $stmt->fetchColumn();
                 if ($ability_id !== false && is_numeric($ability_id)) {
 
-                    $stmt = $db->prepare("INSERT INTO App_Ability (ApplicationID, AbilityID) VALUES (?, ?)");
+                    $stmt = $db->prepare("INSERT INTO Users_Languages (ApplicationID, AbilityID) VALUES (?, ?)");
                     $stmt->execute([$last_id, $ability_id]);
                 } else {
                     error_log("Язык '$language' Не найден в таблице Ability");
